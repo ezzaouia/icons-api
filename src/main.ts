ca6eeef112c6436a7053ca41ccbecf3c74a99953
Mohamed Ezzaouia <ezzaouia.mohamed@gmail.com>
2021-06-02 21:40:55 +0200

    Procfile

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
